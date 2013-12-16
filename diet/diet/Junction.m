e4763ba23b11d39a491e2bdcf3d4f98044722150
morobororo <e115747@ie.u-ryukyu.ac.jp>
2013-12-17 07:30:07 +0900

    create DB for viewDidLoad.

--- diet/diet/Junction.m
+++ diet/diet/Junction.m
@@ -7,6 +7,9 @@
 //
 
 #import "Junction.h"
+#import "FMDatabase.h"
+
+sqlite3* db;
 
 @interface Junction ()
 
@@ -26,8 +29,36 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+
+    [self createDB];
 }
+
+- (void)createDB{
+    
+    //DBファイルのパス
+    NSArray *paths = NSSearchPathForDirectoriesInDomains( NSDocumentDirectory, NSUserDomainMask, YES );
+    NSString *dir   = [paths objectAtIndex:0];
+    
+    //DBファイルがあるかどうか確認
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    
+    if (![fileManager fileExistsAtPath:[dir stringByAppendingPathComponent:@"diet.db"]])
+    {
+        //なければ新規作成
+        FMDatabase *db= [FMDatabase databaseWithPath:[dir stringByAppendingPathComponent:@"diet.db"]];
+        
+        NSString *createCalorieDataTable = @"CREATE TABLE calorie_data (id INTEGER PRIMARY KEY AUTOINCREMENT,calorie_title TEXT,calorie_cal INTEGER,calorie_num INTEGER,date TEXT);";
+        NSString *createExerciseDataTable = @"CREATE TABLE exercise_data (id INTEGER PRIMARY KEY AUTOINCREMENT,exercise_title TEXT,exercise_cal INTEGER,exercise_num INTEGER,date TEXT);";
+        
+        [db open]; //DB開く
+        [db executeUpdate:createCalorieDataTable]; //SQL実行
+        [db executeUpdate:createExerciseDataTable]; //SQL実行
+        [db close];
+    }
+
+    
+}
+
 
 - (void)didReceiveMemoryWarning
 {
