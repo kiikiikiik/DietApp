a468dae4268ed7175dd5fe25a205d40433917b02
morobororo <e115747@ie.u-ryukyu.ac.jp>
2013-12-23 01:29:02 +0900

    select calorie_title complete.

--- diet/diet/CalorieSeek.m
+++ diet/diet/CalorieSeek.m
@@ -16,6 +16,9 @@
 
 @implementation CalorieSeek
 
+@synthesize seekField;
+@synthesize seekResult;
+
 
 NSURL *dirPath;
 FMDatabase *db;
@@ -42,5 +45,46 @@ NSURL *dbPath;
 
 
 - (IBAction)seekButton:(id)sender {
+    
+    NSArray *paths = NSSearchPathForDirectoriesInDomains( NSDocumentDirectory, NSUserDomainMask, YES );
+    
+    NSLog(@"%@",paths);
+    NSString *dir = [paths objectAtIndex:0];
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    
+    if ([fileManager fileExistsAtPath:[dir stringByAppendingPathComponent:@"diet.db"]])
+    {
+        FMDatabase *db= [FMDatabase databaseWithPath:[dir stringByAppendingPathComponent:@"diet.db"]];
+        
+        [db open]; //DB開く
+        
+        FMResultSet *result = [db executeQuery:@"select id,calorie_title from calorie_data"];
+
+
+        NSMutableArray *data= [[NSMutableArray alloc] init];
+
+
+        while ([result next]) {
+            int result_id = [result intForColumn:@"id"];
+            NSString *result_title = [result stringForColumn:@"calorie_title"];
+            [data addObject:result_title];
+            NSLog(@"recode id[%d], title[%@]",result_id, result_title);
+        }
+        
+        NSLog(@"%@", data);
+
+        NSLog(@"count = %d",[data count]);
+
+        
+        // 値は取得できたから、あとは、テーブルへの格納！
+
+
+        
+
+        
+        NSLog(@"Error %@ - %d", [db lastErrorMessage], [db lastErrorCode]);
+        [db close];
+    }
+    
 }
 @end
