1ed11e5b9082f4cb673d20c1721108303e23706d
morobororo <e115747@ie.u-ryukyu.ac.jp>
2014-01-14 02:45:20 +0900

    first stage

---
+++ diet/diet/GetTimeline.h
@@ -0,0 +1,13 @@
+//
+//  GetTimeline.h
+//  diet
+//
+//  Created by Miwa Oshiro on 2014/01/14.
+//  Copyright (c) 2014年 kikukawa haruki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GetTimeline : UIViewController
+
+@end
---
+++ diet/diet/GetTimeline.m
@@ -0,0 +1,38 @@
+//
+//  GetTimeline.m
+//  diet
+//
+//  Created by Miwa Oshiro on 2014/01/14.
+//  Copyright (c) 2014年 kikukawa haruki. All rights reserved.
+//
+
+#import "GetTimeline.h"
+
+@interface GetTimeline ()
+
+@end
+
+@implementation GetTimeline
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
